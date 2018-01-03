01a0053faa1e13a098aa00a61c8d5324450f7168
Michela Zucca <michela.zucca@heig-vd.ch>
2018-01-03 12:04:23 +0100

    Version plus élaborée

--- project/magicHat.py
+++ project/magicHat.py
@@ -1,41 +1,68 @@
-#/usr/bin/python
+# /usr/bin/python
 
 import sys, pygame
 import numpy as np
+from random import randint
 
 pygame.init()
 
 size = width, height = 1024, 728
-speed = [1.0, 1.0]
 black = 0, 0, 0
 
+# Fenêtre du jeu
 screen = pygame.display.set_mode(size)
 
+# Chapeau du magicien
 hat = pygame.image.load('images/magicHat.png')
-hat = pygame.transform.scale(hat, (150,150))
+hat = pygame.transform.scale(hat, (150, 150))
 hatrect = hat.get_rect()
-hatrect.center = (512,600)
+hatrect.center = (512, 600)
 
+# Background du jeu
 background = pygame.image.load('images/background.jpg');
-background = pygame.transform.scale(background, (1024,728))
-
+background = pygame.transform.scale(background, (1024, 728))
 backgroundrect = background.get_rect()
+
+inHat = 0  # Détection de la souris dans le chapeau
+counter = 0  # temps d'affichage de l'objet magique sortie du chapeau
+timeMax = 200  # temps maximum d'affichage
+randomActif = 1  # Sélection d'une nouvelle image
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    inHat = 0
-    outHat = 0
+    # Souris détectée dans le rectangle du chapeau
     if hatrect.collidepoint(pygame.mouse.get_pos()):
         print('Dans le chapeau')
-        magicImage = pygame.image.load('images/1.png')
-        magicImage = pygame.transform.scale(magicImage, (100,100))
-        pos = magicImage.get_rect(center = screen.get_rect().center)
-        screen.blit(magicImage, pos)
-        pygame.display.flip()
-    else :
-        screen.fill(black)
-        screen.blit(background, backgroundrect)
-        screen.blit(hat, hatrect)
-        pygame.display.flip()
+        inHat = 1 # dans le chapeau
+        # tirage de l'image aléatoire
+        if randomActif > 0:
+            no = randint(1, 8)
+            randomActif = 0
+        # chemin d'accès de l'image
+        srcImage = 'images/' + str(no) + '.png'
+    # Souris en dehors du chapeau
+    else:
+        if inHat > 0 and counter < timeMax:
+            # chargement des étoiles autour de l'objet magiques
+            stars = pygame.image.load('images/stars.gif')
+            starsrect = stars.get_rect(center=screen.get_rect().center)
+            # chargement de l'objet magique
+            img = pygame.image.load(srcImage)
+            img = pygame.transform.scale(img, (150, 150))
+            imgrect = img.get_rect(center=screen.get_rect().center)
+            screen.blit(stars, starsrect)
+            screen.blit(img, imgrect)
+            pygame.display.flip()
+            counter += 1
+        else:
+            # reset des variables
+            inHat = 0
+            randomActif = 1
+            counter = 0
+            # affichage de l'écran de jeu de base
+            screen.fill(black)
+            screen.blit(background, backgroundrect)
+            screen.blit(hat, hatrect)
+            pygame.display.flip()
